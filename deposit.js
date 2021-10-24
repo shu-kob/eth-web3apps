e49f01f77d52bc9d711c562052c81acd8a2ce54a
shu-kob <shukobuchi@gmail.com>
2021-10-24 22:15:42 +0900

    remove spaces

--- deposit.js
+++ deposit.js
@@ -4,18 +4,18 @@ const fromAddress = process.env.FROM_ADDRESS;
 const toAddress = process.env.TO_ADDRESS;
 console.log("fromAddress: " + fromAddress);
 
-const Web3 = require('web3');                                                                                                                                                                                                                                                                                                 
+const Web3 = require('web3');
 const ethTx = require('ethereumjs-tx').Transaction;
 
-const web3 = new Web3();   
-// web3.setProvider(new web3.providers.HttpProvider('https://mainnet.infura.io/v3/'+INFURA_API_KEY));     
-web3.setProvider(new web3.providers.HttpProvider('https://ropsten.infura.io/v3/'+INFURA_API_KEY)); 
+const web3 = new Web3();
+// web3.setProvider(new web3.providers.HttpProvider('https://mainnet.infura.io/v3/'+INFURA_API_KEY));
+web3.setProvider(new web3.providers.HttpProvider('https://ropsten.infura.io/v3/'+INFURA_API_KEY));
 
 async function main() {
   // 著名付きトランザクションのnonce値として必要なため、送信元アドレスのTransaction countを取得する
   const count = await web3.eth.getTransactionCount(fromAddress);
   const count16 = count.toString(16);
-  const countHex = `0x${count16}`;  
+  const countHex = `0x${count16}`;
 
   console.log("nonce: " + count);
 
@@ -26,10 +26,10 @@ async function main() {
 
   // transactionのパラメータの設定
   const txParams = {
-    nonce: countHex, 
-    gasPrice: txGasPrice,     
+    nonce: countHex,
+    gasPrice: txGasPrice,
     gasLimit: txGasLimit,
-    to: toAddress, 
+    to: toAddress,
     value: txValue,
     data: null
   };
@@ -41,7 +41,7 @@ async function main() {
   tx.sign(privateKey);
 　　　// Transactionオブジェクトをシリアライズして、16進数で表現
   const serializedTx = tx.serialize();
-  const rawTx = '0x' + serializedTx.toString('hex'); 
+  const rawTx = '0x' + serializedTx.toString('hex');
   console.log("rawTx: " + rawTx);
   // 署名付きトランザクションの送信
   await web3.eth.sendSignedTransaction(rawTx);
